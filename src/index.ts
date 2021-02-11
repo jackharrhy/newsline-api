902be5f5b1ff1e97deafd9cf6a6ef65e773b634a
Jack Arthur Harrhy <me@jackharrhy.com>
2021-02-11 19:22:01 -0330

    parse posts themselves, still just fetching the latest for test purposes, added some todos

--- src/index.ts
+++ src/index.ts
@@ -34,10 +34,20 @@ const parseArchiveIndex = (text : string) : IMonth[] => {
   });
 };
 
+interface IPostDetails {
+  sender: string,
+  from: string,
+  subject: string,
+  contentType: string,
+  text: string,
+  htmlUrl: string,
+  html: string | null,
+}
 interface IPost {
   title: string;
   url: string;
   date: Date;
+  details: IPostDetails | null;
 }
 
 const parsePostInMonth = (li : HTMLLIElement): IPost => {
@@ -48,6 +58,7 @@ const parsePostInMonth = (li : HTMLLIElement): IPost => {
     title: a.text,
     url: `${DOMAIN}${a.href}`,
     date: new Date(dateText),
+    details: null,
   };
 };
 
@@ -61,10 +72,41 @@ const parseMonth = (text : string): IPost[] => {
   });
 };
 
+const parsePost = (text : string): IPostDetails => {
+  const dom = new JSDOM(text);
+  const post = dom.window.document.getElementById('nonprop') as HTMLPreElement;
+
+  const sender = post.querySelector('#MSGHDR-SENDER-PRE')?.textContent as string;
+  const from = (post.querySelector('#MSGHDR-FROM-PRE') as HTMLAnchorElement).textContent as string;
+  const subject = post.querySelector('#MSGHDR-SUBJECT-PRE')?.textContent as string;
+  const contentType = post.querySelector('#MSGHDR-CONTENT-TYPE-PRE')?.textContent as string;
+
+  const postText = post.children[3].textContent as string;
+  const htmlUrl = post.querySelectorAll('a')[1].href;
+
+  return {
+    sender,
+    from,
+    subject,
+    contentType,
+    text: postText,
+    htmlUrl,
+    html: null,
+  };
+};
+
+// things to do
+// - populate posts with posts details
+// - populate a post with its html
+
 (async () => {
   const archiveIndexText = await fetchText(ARCHIVE_INDEX);
   const archiveIndexData = parseArchiveIndex(archiveIndexText);
-  const latest = archiveIndexData[0];
-  const latestText = await fetchText(latest.url);
-  console.log(parseMonth(latestText));
+  const latestMonth = archiveIndexData[0];
+  const latestMonthText = await fetchText(latestMonth.url);
+  const posts = parseMonth(latestMonthText);
+  const latestPost = posts[0];
+  const latestPostText = await fetchText(latestPost.url);
+  const postDetails = parsePost(latestPostText);
+  console.log(postDetails);
 })();
