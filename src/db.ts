4f67d1453663ccf95c302d0ede74d44937b2dadc
Jack Arthur Harrhy <me@jackharrhy.com>
2021-02-12 08:57:27 -0330

    db schema init, might change but it looks good, also not pkey ids, just composite! :)

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,42 @@
 import sqlite3 from "sqlite3"
 import { open } from "sqlite"
 
+const initSql = `
+CREATE TABLE IF NOT EXISTS month (
+    name TEXT PRIMARY KEY,
+    date TEXT NOT NULL,
+	url TEXT NOT NULL,
+	last_updated TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS post (
+	title TEXT NOT NULL,
+    url TEXT NOT NULL,
+    date TEXT NOT NULL,
+    month TEXT NOT NULL REFERENCES month,
+	created TEXT NOT NULL,
+    PRIMARY KEY (title, url)
+);
+
+CREATE TABLE IF NOT EXISTS post_detail (
+    title TEXT NOT NULL REFERENCES post(title),
+    url TEXT NOT NULL REFERENCES post(url),
+    sender TEXT NOT NULL,
+    "from" TEXT NOT NULL,
+    subject TEXT NOT NULL,
+    contenttype TEXT NOT NULL,
+    text TEXT NOT NULL,
+    htmlurl TEXT NOT NULL,
+    html TEXT,
+    PRIMARY KEY (title, url)
+);
+`;
+
 export default async () => {
     const db = await open({
-        filename: 'newsline-api.db',
+        filename: './newsline-api.db',
         driver: sqlite3.Database
     });
-    // await db.exec(`CREATE TABLE`);
+    await db.exec(initSql);
     return db;
 };
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,20 @@ import { ARCHIVE_INDEX } from "./consts";
 import { parseArchiveIndex } from "./parsers/archiveIndex";
 import { parseMonth } from "./parsers/month";
 
+import dbFactory from "./db";
+
 const main = async () => {
+  const db = await dbFactory();
+
+  /*
   const archiveIndexText = await fetchText(ARCHIVE_INDEX);
   const archiveIndexData = parseArchiveIndex(archiveIndexText);
   const latestMonth = archiveIndexData[0];
   const latestMonthText = await fetchText(latestMonth.url);
   const posts = await parseMonth(latestMonthText);
-
   // const postText = await fetchText(url);
   // const details = await parsePost(postText);
+  */
 };
 
 try {
